8058b8428d9737573461d150cfd1713ea03642c1
Pan Wenbin <panwenbin@gmail.com>
2018-05-12 15:48:33 +0800

    add code

---
+++ CorsOptionsFilter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Pan Wenbin <panwenbin@gmail.com>
+ */
+
+namespace panwenbin\yii2\filters;
+
+use Yii;
+use yii\base\ActionFilter;
+
+class CorsOptionsFilter extends ActionFilter
+{
+    public function beforeAction($action)
+    {
+        $request = Yii::$app->getRequest();
+        $response = Yii::$app->getResponse();
+        if ($request->isOptions && $response->headers->has('Access-Control-Allow-Methods')) {
+            // it is CORS preflight request, respond with 200 OK without further processing
+            $response->setStatusCode(200);
+            return false;
+        }
+        return true;
+    }
+}
